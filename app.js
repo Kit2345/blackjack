9d369007f7cdfe1041ef442e8ff8c38e464a1a4c
Alexander <142804583+alexjbrowntech@users.noreply.github.com>
2025-01-13 16:13:41 +0000

    js starter code

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-//Code
+let deck = {"Ace": 1, "Two": 2, "Three": 3, "Four": 4, "Five": 5, "Six": 6, "Seven": 7, "Eight": 8, "Nine": 9, "Ten": 10, "Jack": 10, "Queen": 10, "King": 10};
+
+let gameStats = [];
+
+let player = {hand: [], handValue: 0};
+
+let computer = {hand: [], handValue: 0};
+
+// function calculate hand total
