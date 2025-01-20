187488e6cf989339836926f92ae8614e34b26569
Alexander <142804583+alexjbrowntech@users.noreply.github.com>
2025-01-20 15:25:08 +0000

    created function file

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import { addCard, pushCard, calculateHandTotal } from './functions.js';
+
 let deck = {
   Ace: 1,
   Two: 2,
@@ -20,53 +22,12 @@ let player = { hand: [], handValue: 0 };
 
 let computer = { hand: [], handValue: 0 };
 
-// function calculate hand total
-// Input: Hand which will be an array
-// Method: Iterate through hand, add scores for non-aces, then deal with any aces
-// Output: Total of hand
-
-function calculateHandTotal(hand) {
-  let ace_count = 0;
-  let hand_total = 0;
-  // let status_bust = false
-  for (let i = 0; i < hand.length; i++) {
-    if (hand[i] == "Ace") {
-      ace_count++;
-    }
-    hand_total += deck[hand[i]];
-  }
-  if (ace_count > 0) {
-    if (hand_total + 10 <= 21) {
-      hand_total = hand_total + 10;
-    }
-  }
-  return hand_total;
-}
-
 // Test: check calculateHandTotal function works (should total 15)
 // let hand = ["Five", "Ten", "Ace"];
 // console.log(calculateHandTotal(hand));
 
 // hand = ["Ten", "Ace"];
 // console.log(calculateHandTotal(hand));
-
-// Deal two cards into each array, console log cards and total
-
-//Function to add one card to selected hand
-function addCard(deck) {
-  // get random card
-  const randomIndex = Math.floor(Math.random() * Object.keys(deck).length);
-  const cardKey = Object.keys(deck)[randomIndex];
-  return cardKey;
-}
-
-function pushCard(hand, deck) {
-  new_card = addCard(deck);
-  console.log("new card:", new_card);
-  hand.hand.push(new_card);
-  total = calculateHandTotal(hand.hand);
-  console.log("hand is:", hand.hand, "total is:");
-}
 
 // Test add card function:
 console.log(addCard(deck));
--- functions.js
+++ functions.js
@@ -1,18 +1,37 @@
-// export default function calculateHandTotal(hand, deck) {
-//     let ace_count = 0;
-//     let hand_total = 0;
-//     // let status_bust = false
-//     for (let i = 0; i < hand.length; i++) {
-//       if (hand[i] == "Ace") {
-//         ace_count++;
-//       }
-//       hand_total += deck[hand[i]];
-  
-//       if (ace_count > 0) {
-//         if (hand_total + 10 <= 21) {
-//           hand_total = hand_total + 10;
-//         }
-//       }
-//     }
-//     return hand_total;
-//   }
+//Function to add one card to selected hand
+export function addCard(deck) {
+    // get random card
+    const randomIndex = Math.floor(Math.random() * Object.keys(deck).length);
+    const cardKey = Object.keys(deck)[randomIndex];
+    return cardKey;
+  }
+
+  export function pushCard(user, deck) {
+    let new_card = addCard(deck);
+    console.log("new card:", new_card);
+    user.hand.push(new_card);
+    let total = calculateHandTotal(user.hand, deck);
+    console.log("hand is:", user.hand, "total is:", total);
+  }
+// function calculate hand total
+// Input: Hand which will be an array
+// Method: Iterate through hand, add scores for non-aces, then deal with any aces
+// Output: Total of hand
+
+  export function calculateHandTotal(hand, deck) {
+    let ace_count = 0;
+    let hand_total = 0;
+    // let status_bust = false
+    for (let i = 0; i < hand.length; i++) {
+      if (hand[i] == "Ace") {
+        ace_count++;
+      }
+      hand_total += deck[hand[i]];
+    }
+    if (ace_count > 0) {
+      if (hand_total + 10 <= 21) {
+        hand_total = hand_total + 10;
+      }
+    }
+    return hand_total;
+  }
